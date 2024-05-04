3eaebeb51e767e5872d3d9284eb7d0a2f72a0607
Todd Giles <30381161+eventonz@users.noreply.github.com>
2024-05-05 08:04:31 +1200

    change back to openstrretmaps ahhhhhhh

--- lib/ui/dashboard/athletes_tracking/map_view.dart
+++ lib/ui/dashboard/athletes_tracking/map_view.dart
@@ -111,7 +111,7 @@ class TrackingMapView extends StatelessWidget {
               ),
               children: [
                 TileLayer(
-                  urlTemplate: "https://api.mapbox.com/styles/v1/eventonz/ckbu2fmwe02uw1iqq6gu54h9t/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZXZlbnRvbnoiLCJhIjoiY2lqdDViNTU2MGs0NnRobTU0aTRhanR2ayJ9.Jrzvj7GXCqWrgV3gWiIRsg",
+                  urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
                   subdomains: const ['a', 'b', 'c']
 
                   // evento one pk.eyJ1IjoiZXZlbnRvbnoiLCJhIjoiY2lqdDViNTU2MGs0NnRobTU0aTRhanR2ayJ9.Jrzvj7GXCqWrgV3gWiIRsg
