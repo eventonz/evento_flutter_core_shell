6667b29c8b675b0c985b113b74f28aafa4204678
Todd Giles <30381161+eventonz@users.noreply.github.com>
2024-07-22 14:22:39 +1200

    modified follow and unfollow - added contestID

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,26 +3,27 @@ import 'package:evento_core/evento_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  final eventConfig = AppEventConfig(
-    oneSignalId: '3138d661-ae18-4673-9218-de94842ab543',
-    appName: 'The Race',
-    multiEventListUrl: 'https://eventotracker.com/api/v3/api.cfm/events',
-    multiEventListId: '23',
-    splashImage: 'assets/images/splash_image.png',
-  );
-    
-  
-  /*
-  final eventConfig = AppEventConfig(
+    final eventConfig = AppEventConfig(
     oneSignalId: '31c166a5-3735-4ed7-8bea-b3de9b96a687',
     appName: 'Single Event App',
     singleEventUrl: 'https://eventotracker.com/api/v3/api.cfm/config',
     singleEventId: '91',
     splashImage: 'assets/images/splash_image.png',
   );
+  
+    
+  
+  /*
 
 
 
+final eventConfig = AppEventConfig(
+    oneSignalId: '3138d661-ae18-4673-9218-de94842ab543',
+    appName: 'The Race',
+    multiEventListUrl: 'https://eventotracker.com/api/v3/api.cfm/events',
+    multiEventListId: '23',
+    splashImage: 'assets/images/splash_image.png',
+  );
     
 
 
