a6890f50f789390c3dad8fb83e187ecb0b1d4807
Todd Giles <30381161+eventonz@users.noreply.github.com>
2024-08-12 09:29:16 +1200

    update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,22 +10,39 @@ void main() {
     singleEventId: '91',
     splashImage: 'assets/images/splash_image.png',
   );
+<<<<<<< Updated upstream
 
 
 
 
+=======
+  */
+    
+ final eventConfig = AppEventConfig(
+    oneSignalId: '31c166a5-3735-4ed7-8bea-b3de9b96a687',
+    appName: 'Single Event App',
+    singleEventUrl: 'https://eventotracker.com/api/v3/api.cfm/config',
+    singleEventId: '91',
+    splashImage: 'assets/images/splash_image.png',
+  );
+
+>>>>>>> Stashed changes
 /*
 final eventConfig = AppEventConfig(
     oneSignalId: '3138d661-ae18-4673-9218-de94842ab543',
     appName: 'The Race',
     multiEventListUrl: 'https://eventotracker.com/api/v3/api.cfm/events',
-    multiEventListId: '28',
+    multiEventListId: '23',
     splashImage: 'assets/images/splash_image.png',
   );
+<<<<<<< Updated upstream
 
 */
 
 /*
+=======
+R
+>>>>>>> Stashed changes
 
   final eventConfig = AppEventConfig(
     oneSignalId: '9f4236ce-e028-4c5b-a11d-3ab7a4f7b343',
