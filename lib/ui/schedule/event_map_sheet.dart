34050eb965aa49505726d37dbff03bdbe984295e
Anas Nadeem <anasnadeem200406@gmail.com>
2024-10-25 02:37:02 +0500

    fix schedule

--- lib/ui/schedule/event_map_sheet.dart
+++ lib/ui/schedule/event_map_sheet.dart
@@ -31,120 +31,131 @@ class EventMapSheet extends StatelessWidget {
             ? AppColors.darkBlack
             : AppColors.white,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            AppBar(
-              backgroundColor: Theme.of(context).brightness == Brightness.light
-                  ? AppColors.darkBlack
-                  : AppColors.white,
-              elevation: 0,
-              leading: IconButton(
-                  onPressed: () {
-                    Get.back();
-                  },
-                  icon: Icon(
-                    FeatherIcons.x,
-                    color: AppColors.primary,
-                  )),
-              title: AppText(
-                controller.eventDetails.title!,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  AppText(
-                    controller.getEventTimings(),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  const SizedBox(
-                    height: 2,
-                  ),
-                  AppText(
-                    controller.eventDetails.datetime!
-                        .withDateFormat(format: 'E, dd MMMM'),
-                    color: AppColors.grey,
-                  ),
-                  SizedBox(
-                    height: 2.h,
-                  ),
-                  AppText(
-                    (controller.eventDetails.location?.title ?? ''),
-                  ),
-                  SizedBox(
-                    height: 2.h,
-                  ),
-                  controller.latLng == null
-                      ? const SizedBox()
-                      : SizedBox(
-                          height: 40.h,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(12),
-                            child: Platform.isIOS ? Obx(
-                              () => AppleMap(
-                                rotateGesturesEnabled: false,
-                                zoomGesturesEnabled: false,
-                                pitchGesturesEnabled: false,
-                                scrollGesturesEnabled: false,
-                                initialCameraPosition: CameraPosition(
-
-                                    target: LatLng(controller.latLng!.latitude, controller.latLng!.longitude), zoom: 16),
-                                annotations: Set.of([if(controller.annotation.value != null)controller.annotation.value!]),
-                              ),
-                            ) : MapWidget(
-                              cameraOptions: CameraOptions(
-                                zoom: 16,
-                                center: Point(coordinates: Position(controller.latLng!.longitude, controller.latLng!.latitude)),
-                              ),
-                                gestureRecognizers: Set.of([]),
-                              onMapCreated: (map) async {
-                                var image = await AppHelper.widgetToBytes(Padding(
-                                  padding: const EdgeInsets.only(top: 10.0),
-                                  child: Icon(
-                                    Icons.location_on,
-                                    color: AppColors.primary,
-                                    size: 10.w,
-                                  ),
-                                ));
-                                map.annotations.createPointAnnotationManager().then((value) {
-                                  value.create(PointAnnotationOptions(
-                                    geometry: Point(coordinates: Position(controller.latLng!.longitude, controller.latLng!.latitude)),
-                                    image: image,
-                                  ));
-                                });
-                              },
-                            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    AppBar(
+                      backgroundColor: Theme.of(context).brightness == Brightness.light
+                          ? AppColors.darkBlack
+                          : AppColors.white,
+                      elevation: 0,
+                      leading: IconButton(
+                          onPressed: () {
+                            Get.back();
+                          },
+                          icon: Icon(
+                            FeatherIcons.x,
+                            color: AppColors.primary,
+                          )),
+                      title: AppText(
+                        controller.eventDetails.title!,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          AppText(
+                            controller.getEventTimings(),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
                           ),
-                        )
-                ],
+                          const SizedBox(
+                            height: 2,
+                          ),
+                          AppText(
+                            controller.eventDetails.datetime!
+                                .withDateFormat(format: 'E, dd MMMM'),
+                            color: AppColors.grey,
+                          ),
+                          SizedBox(
+                            height: 2.h,
+                          ),
+                          AppText(
+                            (controller.eventDetails.location?.title ?? ''),
+                          ),
+                          SizedBox(
+                            height: 2.h,
+                          ),
+                          controller.latLng == null
+                              ? const SizedBox()
+                              : SizedBox(
+                                  height: 40.h,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(12),
+                                    child: Platform.isIOS ? Obx(
+                                      () => AppleMap(
+                                        rotateGesturesEnabled: false,
+                                        zoomGesturesEnabled: false,
+                                        pitchGesturesEnabled: false,
+                                        scrollGesturesEnabled: false,
+                                        initialCameraPosition: CameraPosition(
+
+                                            target: LatLng(controller.latLng!.latitude, controller.latLng!.longitude), zoom: 16),
+                                        annotations: Set.of([if(controller.annotation.value != null)controller.annotation.value!]),
+                                      ),
+                                    ) : MapWidget(
+                                      cameraOptions: CameraOptions(
+                                        zoom: 16,
+                                        center: Point(coordinates: Position(controller.latLng!.longitude, controller.latLng!.latitude)),
+                                      ),
+                                        gestureRecognizers: Set.of([]),
+                                      onMapCreated: (map) async {
+                                        var image = await AppHelper.widgetToBytes(Padding(
+                                          padding: const EdgeInsets.only(top: 10.0),
+                                          child: Icon(
+                                            Icons.location_on,
+                                            color: AppColors.primary,
+                                            size: 10.w,
+                                          ),
+                                        ));
+                                        map.annotations.createPointAnnotationManager().then((value) {
+                                          value.create(PointAnnotationOptions(
+                                            geometry: Point(coordinates: Position(controller.latLng!.longitude, controller.latLng!.latitude)),
+                                            image: image,
+                                          ));
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      child: Text(controller.eventDetails.content ?? ''),
+                    ),
+                  ],
+                ),
               ),
             ),
-            const Spacer(),
             controller.latLng == null
                 ? const SizedBox()
                 : SafeArea(
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: Padding(
-                        padding: const EdgeInsets.all(16),
-                        child: CupertinoButton(
-                          color: AppColors.accentLight,
-                          onPressed: () {
-                            AppHelper.showDirectionsOnMap(latLng);
-                          },
-                          padding: const EdgeInsets.all(8),
-                          child: const AppText(
-                            'Get Directions',
-                            color: AppColors.white,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ),
+              child: SizedBox(
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: CupertinoButton(
+                    color: AppColors.accentLight,
+                    onPressed: () {
+                      AppHelper.showDirectionsOnMap(latLng);
+                    },
+                    padding: const EdgeInsets.all(8),
+                    child: const AppText(
+                      'Get Directions',
+                      color: AppColors.white,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
