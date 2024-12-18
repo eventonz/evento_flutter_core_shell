68b903c70352ea486f8bef2d5745ff256d3ec65d
Todd Giles <30381161+eventonz@users.noreply.github.com>
2024-12-18 15:47:33 +1300

    local changes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,7 +9,7 @@ void main() {
    appName: 'Single Event App',
    singleEventUrl: 'https://eventotracker.com/api/v3/api.cfm/config',
    configUrl: 'https://evento-nz.fra1.cdn.digitaloceanspaces.com/config/172.json',
-   singleEventId: '98',
+   singleEventId: '91',
    splashImage: 'assets/images/splash_image.png',
  );
 
