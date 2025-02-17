0471ba199b20bd1bea009c69c95bc69a132fd05a
Todd Giles <30381161+eventonz@users.noreply.github.com>
2025-02-17 17:10:29 +1300

    changed to single version

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:evento_core/evento_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-/*
+
  final eventConfig = AppEventConfig(
    oneSignalId: '31c166a5-3735-4ed7-8bea-b3de9b96a687',
    appName: 'Single Event App',
@@ -12,9 +12,7 @@ void main() {
    singleEventId: '113',
    splashImage: 'assets/images/splash_image.png',
  );
-
-*/
-
+/*
  final eventConfig = AppEventConfig(
     oneSignalId: '111cddd2-0265-44d4-8aa2-af7982884d1b',
     appName: 'Pop Up Races',
@@ -22,7 +20,7 @@ void main() {
     multiEventListId: '27',
     splashImage: 'assets/images/splash_image.png',
   );
-
+*/
 /*
 final eventConfig = AppEventConfig(
    oneSignalId: 'bd954eea-e5dc-4118-8c77-188b5fc1c33d',
